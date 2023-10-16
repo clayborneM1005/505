57f6a9cd36126d1ed06d2a9508e6030ffdcf3c6a
clayborneM1005 <clayborneM1005@students.bowiestate.edu>
2023-10-16 19:12:31 -0400

    Lab2files

---
+++ Lab3.py
@@ -0,0 +1,44 @@
+number1=input('Enter number1')
+number2=input('Enter number2')
+print(number1,number2)
+sum=int(number1)+int(number2)
+print(sum)
+difference=int(number2)-int(number1)
+print(difference)
+multiply=int(number1)*(number2)
+print(multiply)
+division=int(number1)/int(number2)
+print(division)
+print(difference,multiply,division)
+number3=input('Enter number3')
+sum=int(number1)+int(number2)+int(number3)
+print(sum)
+difference=int(number1)-int(number2)-int(number3)
+print(difference)
+multiply=int(number1)*int(number2)*int(number3)
+print(multiply)
+division=int(number1)/int(number2)/int(number3)
+print(division)
+import math
+print(math.pow(9,3))
+print(math.log2(2))
+print(math.sin(10))
+n=7
+k=5
+print(math.comb(n,k))
+print(math.ceil(1.4))
+print(math.cosh(1))
+print(math.erfc(-6))
+print(math.exp(65))
+print(math.expm1(32))
+print(math.degrees(8.90))
+
+
+
+
+
+
+
+
+
+
